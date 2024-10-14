dd7f0b0a902c5fc4c40b6e5e8cc2198689449cf8
Jakub Cernosek <jakub.cernosek1@gmail.com>
2024-10-14 22:30:16 +0200

    Add project files.

---
+++ MauiSchoolApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSchoolApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiSchoolApp/Support.xaml.cs
@@ -0,0 +1,58 @@
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Runtime.Versioning;
+using Microsoft.Maui.Controls;
+
+namespace MauiSchoolApp;
+
+public partial class Support : ContentPage
+{
+    public ObservableCollection<string> NamesList { get; set; }
+    private readonly string filePath;
+
+    public Support()
+    {
+        InitializeComponent();
+        filePath = Path.Combine(FileSystem.AppDataDirectory, "support.txt");
+        NamesList = new ObservableCollection<string>();
+        LoadNamesFromFile();
+        namesListView.ItemsSource = NamesList;
+    }
+
+    private async void OnAddNameClicked(object sender, EventArgs e)
+    {
+        string newName = await DisplayPromptAsync("Add Name", "Enter a name:");
+
+        if (!string.IsNullOrWhiteSpace(newName))
+        {
+            NamesList.Add(newName);
+            SaveNamesToFile();
+        }
+    }
+
+    private void OnDeleteNameClicked(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.BindingContext is string nameToDelete)
+        {
+            NamesList.Remove(nameToDelete);
+            SaveNamesToFile();
+        }
+    }
+
+    private void SaveNamesToFile()
+    {
+        File.WriteAllLines(filePath, NamesList);
+    }
+
+    private void LoadNamesFromFile()
+    {
+        if (File.Exists(filePath))
+        {
+            var namesFromFile = File.ReadAllLines(filePath);
+            foreach (var name in namesFromFile)
+            {
+                NamesList.Add(name);
+            }
+        }
+    }
+}
